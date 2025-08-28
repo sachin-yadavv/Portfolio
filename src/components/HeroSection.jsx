c99393270a93856d62fa10eb09c9dd54594149dc
sachin-yadavv <ysachin120504@gmail.com>
2025-08-28 20:21:15 +0530

    inital commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,54 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+              {" "}
+              Sachin
+            </span>
+            {/* <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+              {" "}
+              Machado
+            </span> */}
+          </h1>
+
+          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+             A web developer passionate about front-end design, 
+             competitive programming, and 
+             solving real-world problems through clean, efficient code
+          </p>
+          
+           <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+           <a href="#projects" className="cosmic-button">
+              View My Work
+            </a>
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href=""
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+        <ArrowDown className="h-5 w-5 text-primary" />
+      </div>
+    </section>
+  );
+};
